1cd62fd0c57709f88879399779a41f81b7bf1fee
Antonio Natali <agoognat@gmail.com>
2021-10-18 19:16:51 +0200

    servlet

--- DynamicWeb1/WebContent/firstJsp.jsp
+++ DynamicWeb1/WebContent/firstJsp.jsp
@@ -49,7 +49,7 @@
  The form-handler is typically a file on the server with a script for processing input data.
  <!-- curl -d "{\"fname\":\"mario\", \"lname\": rossi}"  -X POST  raspberrypi.local:8080/SampleWebApp/actionpage.jsp -->       
 
- <form action="/SampleWebApp/actionpage.jsp" method="post">
+ <form action="actionpage.jsp" method="post">
   <label for="fname">First name:</label><br>
   <input type="text" id="fname" name="fname" value="John"><br>
   <label for="lname">Last name:</label><br>
--- DynamicWeb1/src/action/FirstServlet.java
+++ DynamicWeb1/src/action/FirstServlet.java
@@ -2,7 +2,18 @@
  * https://www.html.it/articoli/tomcat-lapplicazione-servita/
  * http://localhost:8080/manager/html
  * 
- * 
+Java EE 6 spec (in general) and Servlet 3.0 spec (in particular) attempt 
+to de-emphasize deployment descriptors.
+
+You can use annotation to provide all the data that had been included in the web.xml file. 
+You should read through the Servlet 3.0 spec from the jcp.org site to get a bit 
+more explanatory text.
+
+To change the url-mapping for a Servlet 3.0 servlet, the first place to look 
+is in the source code for the servlet. Look for (and change) the value of the 
+urlPatterns element.
+
+If you are trying to create a web app based on Servlet 3.0, try to avoid creating a web.xml file.
  */
 package action;
 
@@ -36,14 +47,17 @@ public class FirstServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println(""+request.getContextPath());
-		//http://localhost:8080/DynamicWeb1/FirstServlet?to=Dilogy
-		String toName = request.getParameter("to");
+		System.out.println("request.getContextPath()="+request.getContextPath());
+		//http://localhost:8080/DynamicWeb1/FirstServlet?firstname=Mario&lasttname=Rossi
+		String firstname = request.getParameter("firstname");
+		String lasttname = request.getParameter("lastname");
+		System.out.println("firstname="+firstname+" lasttname="+lasttname);
+		
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<head><title>HelloServlet</title></head>");
-		out.println("<body>Hello to "+toName+"! from servlet</body>");
+		out.println("<body>Hello "+firstname + " " + lasttname + "! from FirstServlet</body>");
 		out.println("</html>");		
 	}
 
